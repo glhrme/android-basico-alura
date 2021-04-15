ed8a9678885b2d00aad0fffb1a0f4d1bea7bea47
Guilherme dos Santos <guilherme@guisantos.com.br>
2021-04-15 20:20:53 -0300

    Oi Mundo com Toasts

--- app/src/main/java/br/com/guisantos/primeiroaplicativoalura/MainActivity.java
+++ app/src/main/java/br/com/guisantos/primeiroaplicativoalura/MainActivity.java
@@ -1,9 +1,18 @@
 package br.com.guisantos.primeiroaplicativoalura;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity {
-    public static void main(String[] args) {
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Toast.makeText(this, "Oi", Toast.LENGTH_SHORT).show();
     }
+
 }
