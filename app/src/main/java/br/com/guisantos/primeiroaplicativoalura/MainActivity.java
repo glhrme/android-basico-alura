bf27652675fe49ee7d1da8a34a28fdbf195be32f
Guilherme dos Santos <guilherme@guisantos.com.br>
2021-04-15 20:14:13 -0300

    Initial

---
+++ app/src/main/java/br/com/guisantos/primeiroaplicativoalura/MainActivity.java
@@ -0,0 +1,9 @@
+package br.com.guisantos.primeiroaplicativoalura;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+    public static void main(String[] args) {
+
+    }
+}
